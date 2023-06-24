7a87ad72ddc3d93d05b38fb8fc079920a651fd41
Redtiger777 <redtiger3777@gmail.com>
2023-06-24 12:58:23 +0200

    converted button group component to ts

--- client/src/components/BaseButtonGroup.vue
+++ client/src/components/BaseButtonGroup.vue
@@ -1,50 +1,52 @@
 <template>
-    <div :style='getStyle'>
-        <slot></slot>
+    <div :style="getStyle">
+      <slot></slot>
     </div>
-</template>
+  </template>
   
-<script>
-export default {
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
     props: {
-        mode: {
-            type: String,
-            default: 'column'
-        }
+      mode: {
+        type: String,
+        default: 'column',
+      },
     },
     computed: {
-        getStyle() {
-            return {
-                'flex-direction': this.mode,
-            };
-        }
-    }
-};
-</script>
-
-<style scoped>
-div {
+      getStyle(): Record<string, string> {
+        return {
+          'flex-direction': this.mode,
+        };
+      },
+    },
+  });
+  </script>
+  
+  <style scoped>
+  div {
     display: flex;
     justify-content: space-between;
     gap: 1rem;
-
+  
     margin-top: 1rem;
     width: 100%;
     max-width: 40rem;
-}
-
-@media screen and (max-width: 33rem) {
+  }
+  
+  @media screen and (max-width: 33rem) {
     div {
-        gap: 0.75rem;
-        margin-top: 0.75rem;
-
+      gap: 0.75rem;
+      margin-top: 0.75rem;
     }
-}
-
-@media screen and (max-width: 300px) {
+  }
+  
+  @media screen and (max-width: 300px) {
     div {
-        gap: 0.5rem;
-        margin-top: 0.5rem;
+      gap: 0.5rem;
+      margin-top: 0.5rem;
     }
-}
-</style>
+  }
+  </style>
+  
