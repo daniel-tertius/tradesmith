079d5aeb40268385fd259b96d735ade4e2835932
Redtiger777 <redtiger3777@gmail.com>
2023-11-01 20:01:05 +0200

    move types to its own file

---
+++ server/api/types/types.ts
@@ -0,0 +1,33 @@
+export type statuses = "buy" | "wait" | "kill"
+export type optionsType = {
+    status?: statuses,
+    idleTime: number,
+    lunoKey: string,
+    lunoSecret: string,
+    btcTradeAmount: number,
+    btcGapBetweenBuys: number,
+    profitPercentage: number
+    waitForValue?: {
+        buy: number,
+        sell: number
+    },
+    buyPrices?: number[],
+    isDebugging?: boolean
+}
+
+export type lunoOrder = {
+        "base": string,
+        "completed_timestamp": string,
+        "counter": string,
+        "creation_timestamp": string,
+        "expiration_timestamp": string,
+        "fee_base": string,
+        "fee_counter": string,
+        "limit_price": string,
+        "limit_volume": string,
+        "order_id": string,
+        "pair": string,
+        "state": "PENDING" | "COMPLETE",
+        "time_in_force": string,
+        "type": "BUY" | "SELL"
+    }
