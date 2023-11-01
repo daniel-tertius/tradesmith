b51eea424c31f70c823e64aefd6efac66be8d95d
Redtiger777 <redtiger3777@gmail.com>
2023-11-01 20:04:30 +0200

    UPdated test cases

--- test_cases/index.ts
+++ test_cases/index.ts
@@ -1,15 +1,16 @@
 import Tradesmith from "../server/api/trader/TradeSmith";
-const LUNO_API_KEY="ckt4zur6teytt"
-const LUNO_API_SECRET="DN64ILFTcbuubHO0cBQQsQCgUXFBfN6BPCk1feaVMdo"
+const LUNO_API_KEY = "rjpbbwu397rm"
+const LUNO_API_SECRET = "7duRqBIjaByrFlIjeoF8kctkjxoPLiWwqN3utCADcLw"
 
 function run() {
     const tradesmith = new Tradesmith({
         btcGapBetweenBuys: 0.0001,
         btcTradeAmount: 0.0001,
-        idleTime: 30 * 1000,
+        idleTime: 10 * 1000,
         profitPercentage: 1,
-        lunoKey: "",
-        lunoSecret: ""
+        lunoKey: LUNO_API_KEY,
+        lunoSecret: LUNO_API_SECRET,
+        isDebugging: true
     });
 
     tradesmith.start();
